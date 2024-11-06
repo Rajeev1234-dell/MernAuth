f1c6717c86b5f42c6bddecd890a60ac2a2a5edf0
Rajeev Kumar <pmdbrajeev1@gmail.com>
2024-11-06 11:41:38 +0530

    Backend entry point

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { connectDB } from "./db/connectDB.js";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000
+
+app.use(express.json())
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log("Server is listening");
+})
