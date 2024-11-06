464b181a48aebf9d7f2900e977cf3eb79299e73b
Rajeev Kumar <pmdbrajeev1@gmail.com>
2024-11-06 11:39:21 +0530

    Added user routes

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,14 @@
+import express from "express"
+import { login, logout, signup, verifyEmail } from "../controllers/auth.controller.js";
+
+const router = express.Router();
+
+router.post('/signup', signup)
+
+router.get('/login', login)
+
+router.post('/logout', logout)
+
+router.post("/verify-email", verifyEmail)
+
+export default router;
