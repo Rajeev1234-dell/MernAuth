bd4f5ce24433ee357d027f7778d9291570c2fb8b
Rajeev Kumar <pmdbrajeev1@gmail.com>
2024-11-08 11:55:29 +0530

    Added Protect Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,44 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import FloatingShape from "./components/FloatingShape";
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
 import EmailVerificationPage from "./pages/EmailVerificationPage";
 import { Toaster } from "react-hot-toast";
+import { useAuthStore } from "./store/authStore";
+import { useEffect } from "react";
+import DashboardPage from "./pages/DashboardPage";
+
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, user } = useAuthStore();
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+  if (!user.isVerified) {
+    return <Navigate to="/login" replace />;
+  }
+  return children;
+};
+const RedirectAuthenticationUser = ({ children }) => {
+  const { isAuthenticated, user } = useAuthStore();
+  console.log("User", user);
+
+  if (isAuthenticated && user.isVerified) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};
 
 function App() {
+  const { isCheckingAuth, checkAuth, isAuthenticated, user } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  // console.log("isauthenticated", isAuthenticated);
+  // console.log("user", user);
+
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
       <FloatingShape
@@ -31,9 +64,30 @@ function App() {
       />
 
       <Routes>
-        <Route path="/" element={"Home"} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <RedirectAuthenticationUser>
+              <SignupPage />
+            </RedirectAuthenticationUser>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RedirectAuthenticationUser>
+              <LoginPage />
+            </RedirectAuthenticationUser>
+          }
+        />
         <Route path="/verify-email" element={<EmailVerificationPage />} />
       </Routes>
       <Toaster />
--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -77,6 +77,7 @@ export const useAuthStore = create((set) => ({
     set({ isLoading: true, error: null });
     try {
       const response = await axios.post(`${API_URL}/verify-email`, { code });
+
       set({
         user: response.data.user,
         isAuthenticated: true,
@@ -92,19 +93,20 @@ export const useAuthStore = create((set) => ({
     }
   },
 
-  //   checkAuth: async () => {
-  //     set({ isCheckingAuth: true, error: null });
-  //     try {
-  //       const response = await axios.get(`${API_URL}/check-auth`);
-  //       set({
-  //         user: response.data.user,
-  //         isAuthenticated: true,
-  //         isCheckingAuth: false,
-  //       });
-  //     } catch (error) {
-  //       set({ error: null, isCheckingAuth: false, isAuthenticated: false });
-  //     }
-  //   },
+  checkAuth: async () => {
+    set({ isCheckingAuth: true, error: null });
+    try {
+      const response = await axios.get(`${API_URL}/check-auth`);
+
+      set({
+        user: response.data.User,
+        isAuthenticated: true,
+        isCheckingAuth: false,
+      });
+    } catch (error) {
+      set({ error: null, isCheckingAuth: false, isAuthenticated: false });
+    }
+  },
 
   //   forgotPassword: async (email) => {
   //     set({ isLoading: true, error: null });
